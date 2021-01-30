0b30ec238e6a307de85ef59c27c91e0790a2d725
Mick Tesla <52467281+mick-tesla@users.noreply.github.com>
2021-01-30 19:37:55 +0000

    Update country.py

--- country.py
+++ country.py
@@ -1,40 +1,33 @@
 #!/usr/bin/python3
 import os
-
-#Default
-
-os.chdir('Domains')
-os.mkdir('inf')
-file_list=os.listdir('.')
+	from os import chdir,mkdir,listdir,system
+chdir('Domains')
+try:mkdir('inf_200')
+except:pass
+def file_list():
+	system("ls |grep '.txt' > .flist")
+	l=open(".flist","r").read().split('\n')
+	l.remove('')
+	return l
 types=[]
-
-#get domain type
-
-def Dom(ml):
-	ml=ml.replace('.txt','') #gmail.com
-	mlF=ml.split('.') # ['gmail','com']
-	try: mlF.remove('')
-	except: pass
-	mlForm=mlF[-1] # 'com'
-	return mlForm
-
-#get len on maillist
-
-def gelLen(ml):
-	return len(open(ml).read().split('\n'))
-
-#filter by country
-
-def sort(ml):	#gmail.com.txt
-	if Dom(ml) in types:
-		os.system('mv '+ml+' '+mlForm+'/')
-	else:
-		os.mkdir(Dom(ml))
-		types.append(ml)
-		sort(ml)
-
-#main
-
-for file in file_list:
-	try: sort(file)
+def getDom(fle):
+	return fle.split('.')[-2]
+def getLen(ml):
+	a=open(ml,'r').read().split('\n')
+	return len(a)
+def sort(file):
+	a=getDom(file)
+	system('mkdir '+a)
+	try:
+		if a==types[-1]:
+			system('mv '+file+' '+a+'/')
+		else:
+			types.append(a)
+			sort(file)
+	except IndexError:
+		types.append(a)
+		sort(file)
+fl=file_list()
+for file in fl:
+	try:sort(file)
 	except Exception as e: print(e)
